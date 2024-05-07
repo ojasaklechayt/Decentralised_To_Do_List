c1d78888fd26f49becf6ae06b1f9ed9f92939dd3
Ojas Aklecha <ojasaklechayt@gmail.com>
2024-05-07 16:45:24 +0530

    Improved error handling

--- app/api/tasks/route.js
+++ app/api/tasks/route.js
@@ -5,10 +5,23 @@ const prisma = new PrismaClient();
 export const GET = async () => {
     try {
         const tasks = await prisma.tasks.findMany();
-        return new Response(JSON.stringify(tasks), { status: 200 })
+        return new Response(JSON.stringify(tasks), { status: 200 });
     } catch (error) {
-        console.error(error);
-        return new Response("Failed to fetch all tasks", { status: 500 })
+        let errorMessage = "";
+        let statusCode = 500;
+
+        if (error instanceof Prisma.PrismaClientKnownRequestError) {
+            console.error("Prisma error:", error.message);
+            errorMessage = "Failed to fetch all tasks: Database error";
+        } else if (error instanceof Prisma.PrismaClientInitializationError) {
+            console.error("Prisma initialization error:", error.message);
+            errorMessage = "Failed to fetch all tasks: Prisma initialization error";
+        } else {
+            console.error("Error fetching tasks:", error);
+            errorMessage = "Failed to fetch all tasks: " + error.message;
+        }
+
+        return new Response(errorMessage, { status: statusCode });
     }
 }
 
