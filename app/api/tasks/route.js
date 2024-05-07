bf60709f1300f159df1948f6216079ce0a9998b2
Ojas Aklecha <ojasaklechayt@gmail.com>
2024-05-07 15:46:18 +0530

    Prisma Code Update

--- app/api/tasks/route.js
+++ app/api/tasks/route.js
@@ -1,11 +1,9 @@
 // app/api/tasks/index.js
-import Tasks from "@/models/notes";
-import { connectToDatabase } from "@/utils/mongodb";
-
+import { PrismaClient } from '@prisma/client'
+const prisma = new PrismaClient();
 export const GET = async () => {
     try {
-        await connectToDatabase();
-        const tasks = await Tasks.find({});
+        const tasks = await prisma.tasks.findMany();
         return new Response(JSON.stringify(tasks), { status: 200 })
     } catch (error) {
         console.error(error);
@@ -17,9 +15,7 @@ export const POST = async (request) => {
     try {
         const { title, description, time, category, completed } = await request.json();
         console.log("Hello", title, description, time, category, completed);
-        await connectToDatabase();
-        const task = new Tasks({ title, description, time, category, completed });
-        await task.save();
+        const task = new prisma.tasks.create({ data: { title, description, time, category, completed } });
         return new Response(JSON.stringify(task), { status: 201 })
     } catch (error) {
         console.error(error);
--- utils/mongodb.js
+++ utils/mongodb.js
@@ -12,13 +12,13 @@ export const connectToDatabase = async () => {
     console.log('=> using new database connection');
 
     try {
-        const db = await mongoose.connect(process.env.MONGODB_URI, {
+        const db = await mongoose.connect(process.env.DATABASE_URL, {
             useNewUrlParser: true,
             useUnifiedTopology: true,
         });
 
         isConnected = true;
-        
+
         console.log("=> Database connected");
     } catch (error) {
         console.error(`Unable to connect to database: ${error}`);
